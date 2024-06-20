3736a67f088bb15c53c96e7976489879d9d754cf
Mahmoudbaky <ma7mouudbaky@gmail.com>
2024-06-20 21:35:33 +0300

    add:deploy wrokflow

--- src/Components/TheMenu.jsx
+++ src/Components/TheMenu.jsx
@@ -1,11 +1,5 @@
-import Placeholder from "react-bootstrap/Placeholder";
-
 function TheMenu() {
-  return (
-    <>
-      <Placeholder xs={6} />
-    </>
-  );
+  return <></>;
 }
 
 export default TheMenu;
